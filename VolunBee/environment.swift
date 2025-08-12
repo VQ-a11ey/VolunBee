96fe2786220946f11da06c1f35a1836ede77f37e
Arohi Pokhrel <arohipokhrel32@gmail.com>
2025-08-12 15:45:49 -0700

    Added Environment file

---
+++ VolunBee/environment.swift
@@ -0,0 +1,50 @@
+//
+//  environment.swift
+//  VolunBee
+//
+//  Created by Scholar on 8/12/25.
+//
+
+import SwiftUI
+
+struct environment: View {
+    var body: some View {
+        ZStack {
+            Image("yellowBackground")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fill)
+                .padding(.top, -5.0)
+                .ignoresSafeArea()
+            
+            
+            VStack (alignment: .leading, spacing: 20) {
+                
+                Text("Environment")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.leading)
+                    .padding(.trailing, 120)
+                
+                Text("")
+                    .font(.title2)
+                Text("")
+                    .font(.title2)
+                Text("")
+                    .font(.title2)
+                Text("")
+                    .font(.title2)
+                    Text("")
+                    .font(.title2)
+                
+                
+                
+                
+            }//end VStack
+            .padding()
+        }//end ZStack
+    }//end body
+}//end struct
+
+#Preview {
+    environment()
+}
