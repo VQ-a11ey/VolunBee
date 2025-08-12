0ba561b1ef86f7dc9e35091be0e8336bdfff2190
Vicky Qin <vickyqin2010@gmail.com>
2025-08-12 11:13:49 -0700

    Initial Commit

---
+++ VolunBee/VolunBeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolunBeeApp.swift
+//  VolunBee
+//
+//  Created by Scholar on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct VolunBeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
