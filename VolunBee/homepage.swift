7f518bfe85cb3cbefebbfe124cd46356b99d66de
Vicky Qin <vickyqin2010@gmail.com>
2025-08-12 15:16:11 -0700

    Woking dropdown with links

--- VolunBee/homepage.swift
+++ VolunBee/homepage.swift
@@ -10,8 +10,7 @@ import SwiftUI
 struct homepage: View {
    @Namespace private var animationNamespace
     @State private var expanded = false
-    @State private var select = "Choose a category"
-    let options = ["Animal Shelters", "Soup Kitchen", "Elderly Home"]
+    
     var body: some View {
         ZStack{
             Image("background")
@@ -39,17 +38,24 @@ struct homepage: View {
                 TextField("Search...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
                     .border(Color.gray, width:1)
                     .font(.title)
-                Button(action: {
-                    withAnimation(.spring()) {)
-                
+                NavigationView {
+                    VStack{
+                        DisclosureGroup("Categories", isExpanded: $expanded){
+                            Text("Hi")
+                            NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                                Text("Animal Shelters")
+                            }
+                        }
+                        }
+                        }
+                        }
+                    }
                 }
                 }
-            } //vstack
-            .padding()
-                
-        }
-    }
-}
+             //vstack
+
+        
+    
 
 #Preview {
     homepage()
