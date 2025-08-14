4f89e7921d31b739fba0552678e80ca576a1755f
Vicky Qin <vickyqin2010@gmail.com>
2025-08-14 10:33:45 -0700

    making more things pretty, fooddrive enviorment done

--- VolunBee/fooddrive.swift
+++ VolunBee/fooddrive.swift
@@ -14,42 +14,69 @@ struct fooddrive: View {
                 Image("yellowBackground")
                     .resizable(resizingMode: .stretch)
                     .aspectRatio(contentMode: .fill)
-                    .padding(.top, -5.0)
                     .ignoresSafeArea()
                 
                 
-                VStack (alignment: .leading, spacing: 40) {
+                VStack (alignment: .leading, spacing: 20) {
                     Spacer()
                     Spacer()
-                    Text("Food Drives")
+                    Text("Food Drives\n")
                         .font(.largeTitle)
                         .fontWeight(.bold)
+                        .foregroundColor(Color("brownish"))
                         .multilineTextAlignment(.leading)
                         .padding(.trailing, 120)
                     NavigationLink(destination: SFMarinFoodBank()){
                         Text("San Francisco/Marin Food Bank")
                     }
+                    .frame(width: 350.0, height: 30.0)
+                    .background(Color.dusk)
+                    .foregroundColor(.skin)
+                    .cornerRadius(8)
                     .font(.title2)
                     NavigationLink(destination: TheNewPantry()) {
                         Text("The Food Pantry")}
                         .font(.title2)
+                        .frame(width: 350.0, height: 30.0)
+                        .background(Color.skin)
+                        .foregroundColor(.brownish)
+                        .cornerRadius(8)
                     NavigationLink(destination: MissionFoodHub()) {
                         Text("Mission Food Hub")
                     }
-                        .font(.title2)
+                    .frame(width: 350.0, height: 30.0)
+                    .background(Color.dusk)
+                    .foregroundColor(.skin)
+                    .cornerRadius(8)
+                    .font(.title2)
                     NavigationLink(destination: YMCA()){
                         Text("YMCA's Food Assistance")
                     }
+                    .frame(width: 350.0, height: 30.0)
+                    .background(Color.skin)
+                    .foregroundColor(.brownish)
+                    .cornerRadius(8)
                         .font(.title2)
                     NavigationLink(destination: feedingAmerica()){
                         Text("Feeding America")
                     }
+                    .frame(width: 350.0, height: 30.0)
+                    .background(Color.dusk)
+                    .foregroundColor(.skin)
+                    .cornerRadius(8)
                     .font(.title2)
-                    Text("YMCA's Food Assistance")
+                    Text("JFCS Food Banks")
                         .font(.title2)
-                    Text("Feeding America")
+                        .frame(width: 350.0, height: 30.0)
+                        .background(Color.skin)
+                        .foregroundColor(.brownish)
+                        .cornerRadius(8)
+                    Text("Project Open Hand")
                         .font(.title2)
-                    
+                        .frame(width: 350.0, height: 30.0)
+                        .background(Color.dusk)
+                        .foregroundColor(.skin)
+                        .cornerRadius(8)
                     VStack {
                         Image("flyingbee")
                             .resizable(capInsets: EdgeInsets(top: -200.0, leading: 100.0, bottom: 10.0, trailing: 100.0), resizingMode: .stretch)
